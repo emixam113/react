9e1adbede7aa44e18b3719a45b38485946ef9efb
emixam113 <98613290+emixam113@users.noreply.github.com>
2024-04-02 12:29:19 +0200

    j'aime réact

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,21 +1,7 @@
-const pokemonList = [
-    {
-        name: "bulbasaur", 
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    }, 
-    {
-        name:"mew",
-    },
-
-];
-
-function PokemonCards() {
-    const pokemon = pokemonList[0];
+function PokemonCards({pokemon}){
     return (<figure>{pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>???</p>}
     <figcaption>{pokemon.name}</figcaption>
-    </figure>)
+    </figure>)   
 }
-
-
 
 export default PokemonCards;
