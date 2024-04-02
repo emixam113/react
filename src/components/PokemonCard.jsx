bc482392614611532b403669ee26cc6bcf8971be
emixam113 <98613290+emixam113@users.noreply.github.com>
2024-04-02 16:34:38 +0200

    évolution

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,16 @@
+import PropTypes from "prop-types"
 function PokemonCards({pokemon}){
     return (<figure>{pokemon.imgSrc ? <img src={pokemon.imgSrc}/> : <p>???</p>}
     <figcaption>{pokemon.name}</figcaption>
     </figure>)   
+}
+
+PokemonCards.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired, 
+        imgSrc: PropTypes.string
+    }).isRequired
+
 }
 
 export default PokemonCards;
