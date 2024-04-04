7d78bb2f5addc0a7ba236d6426e075507517082e
emixam113 <98613290+emixam113@users.noreply.github.com>
2024-04-04 10:10:27 +0200

    i've initialize new pokemon and disable button next and previous

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,46 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
-  //const [count, setCount] = useState(0)
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+ const handleClick = (event) => {
+  
+event.target.textContent === 'Précédent' ? setPokemonIndex(pokemonIndex-1): setPokemonIndex(pokemonIndex+1);
+ }
 
   return (
    <div>
-      <PokemonCard pokemon ={pokemonList[0]}/>
-   </div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+
+    {pokemonIndex > 0 ? <button onClick = {handleClick}>Précédent</button> : <button onClick = {handleClick} disabled>Précédent</button>}
+
+    {pokemonIndex < pokemonList.length-1 ? <button onClick = {handleClick}>Suivant</button>: <button onClick = {handleClick} disabled >Suivant</button> }
+      <p>{pokemonIndex}</p>
+
+    </div>
   )
 }
 
 const pokemonList = [
   {
-      name: "bulbasaur", 
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      name: "Bulbasaur", 
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png", //pokemon 1;
   }, 
   {
-      name:"mew",
+    name: "Ivysaur", 
+    imgSrc:"http://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png", //pokemon 2; 
+  }, 
+  {
+    name: "Squirtle", 
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png", //pokemon 3; 
+  }, 
+  {
+    name: "Pikachu",
+    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png", // pokemon 4; 
+  },
+  {
+      name:"Mew", //pokemon 4; 
+      imgSrc:"https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
   },
 
 ];
